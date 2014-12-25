a4fcdc6794961a6f877df54d037f72486753562c
Miika Sirén <miika.siren@cs.helsinki.fi>
2014-12-25 14:07:55 +0200

    move the adapter to it's own file

--- app/src/main/java/com/devglyph/reitittaja/JourneyPlannerFragment.java
+++ app/src/main/java/com/devglyph/reitittaja/JourneyPlannerFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
@@ -14,36 +13,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.TimePicker;
-import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 
 /**
@@ -54,7 +34,7 @@ import java.util.List;
  * Use the {@link JourneyPlannerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class JourneyPlannerFragment extends Fragment implements AdapterView.OnItemClickListener {
+public class JourneyPlannerFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -66,6 +46,7 @@ public class JourneyPlannerFragment extends Fragment implements AdapterView.OnIt
     private OnFragmentInteractionListener mListener;
 
     private final String LOG_TAG = JourneyPlannerFragment.class.getSimpleName();
+    private ArrayList<Location> locationList = new ArrayList<>();
 
     private boolean mDeparture = true;
     private boolean[] mTransportationModeStates = new boolean[8];
@@ -88,6 +69,10 @@ public class JourneyPlannerFragment extends Fragment implements AdapterView.OnIt
     private int mMinutes = -1;
     private int mMonth = -1;
     private int mDay = -1;
+
+    private Location startLocation;
+    private Location endLocation;
+
 
     /**
      * Use this factory method to create a new instance of
@@ -161,27 +146,28 @@ public class JourneyPlannerFragment extends Fragment implements AdapterView.OnIt
         mDateButton.setText(R.string.journey_planner_now_date);
 
         mStartPlace = (AutoCompleteTextView) view.findViewById(R.id.start_place);
-        mStartPlace.setAdapter(new PlacesAutoCompleteAdapter(getActivity(), R.layout.list_item));
-        //createOnItemClickListener(this,);
-        //createStartPlaceListener(mStartPlace);
-        //mStartPlace.setOnClickListener();
+        mStartPlace.setAdapter(new PlacesAutoCompleteAdapter(this, getActivity(), R.layout.list_item));
+        createOnItemClickListener(mStartPlace);
 
         mEndPlace = (AutoCompleteTextView) view.findViewById(R.id.end_place);
-        //createEndPlaceListener(mEndPlace);
-        mEndPlace.setAdapter(new PlacesAutoCompleteAdapter(getActivity(), R.layout.list_item));
-        //mEndPlace.setOnClickListener();
+        mEndPlace.setAdapter(new PlacesAutoCompleteAdapter(this, getActivity(), R.layout.list_item));
+        createOnItemClickListener(mEndPlace);
 
         return view;
     }
 
-    private void createOnItemClickListener(AdapterView<?> adapterView, View view, int position, long id) {
-        String str = (String) adapterView.getItemAtPosition(position);
-        Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+    private void createOnItemClickListener(final AutoCompleteTextView autoCompleteTextView) {
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (autoCompleteTextView.getId() == R.id.start_place) {
+                    startLocation = getLocationList().get(position);
+                }
+                else if (autoCompleteTextView.getId() == R.id.end_place) {
+                    endLocation = getLocationList().get(position);
+                }
+            }
+        });
     }
 
     private void createRadioButtonChangeListener(RadioGroup group) {
@@ -423,31 +409,6 @@ public class JourneyPlannerFragment extends Fragment implements AdapterView.OnIt
         mMonth = -1;
     }
 
-    private void createStartPlaceListener(EditText text) {
-        text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    Log.d(LOG_TAG, "start place");
-                    onButtonPressed("start");
-                }
-
-            }
-        });
-    }
-
-    private void createEndPlaceListener(EditText text) {
-        text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    Log.d(LOG_TAG, "end place");
-                    onButtonPressed("end");
-                }
-            }
-        });
-    }
-
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(String string) {
         if (mListener != null) {
@@ -487,297 +448,12 @@ public class JourneyPlannerFragment extends Fragment implements AdapterView.OnIt
         public void onFragmentInteraction(String string);
     }
 
-
-
-    private class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
-        private ArrayList<String> resultList;
-
-        public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
-            super(context, textViewResourceId);
-        }
-
-        @Override
-        public int getCount() {
-            return resultList.size();
-        }
-
-        @Override
-        public String getItem(int index) {
-            return resultList.get(index);
-        }
-
-        @Override
-        public Filter getFilter() {
-            Filter filter = new Filter() {
-                @Override
-                protected FilterResults performFiltering(CharSequence constraint) {
-                    FilterResults filterResults = new FilterResults();
-                    if (constraint != null) {
-                        // Retrieve the autocomplete results.
-                        Log.d(LOG_TAG, "Charconstraintsequence "+constraint);
-
-                        resultList = autocomplete(constraint.toString());
-
-                        // Assign the data to the FilterResults
-                        filterResults.values = resultList;
-                        filterResults.count = resultList.size();
-
-                        /*
-                        ArrayList<String> list = autocomplete(constraint.toString());
-
-                        if (list != null && !resultList.isEmpty()) {
-                            resultList = list;
-
-                            // Assign the data to the FilterResults
-                            filterResults.values = resultList;
-                            filterResults.count = resultList.size();
-                        }
-                        */
-                    }
-                    return filterResults;
-                }
-
-                @Override
-                protected void publishResults(CharSequence constraint, Filter.FilterResults results) {
-                    if (results != null && results.count > 0) {
-                        notifyDataSetChanged();
-                    }
-                    else {
-                        notifyDataSetInvalidated();
-                    }
-                }};
-            return filter;
-        }
+    public ArrayList<Location> getLocationList() {
+        return locationList;
     }
 
-    private ArrayList<String> autocomplete(String input) {
-        if (input == null || input.length() < 3) {
-            return null;
-        }
-
-        ArrayList<String> autoCompleteResultsList = new ArrayList<>();
-
-        HttpURLConnection conn = null;
-        StringBuilder jsonResults = new StringBuilder();
-        String resultString;
-        BufferedReader reader = null;
-        try {
-            final String QUERY_BASE_URL =
-                    "http://api.reittiopas.fi/hsl/prod/?";
-            final String QUERY_PARAM = "request";
-            final String FORMAT_PARAM = "format";
-            final String USERNAME_PARAM = "user";
-            final String PASSWORD_PARAM = "pass";
-            final String COORDINATE_OUTPUT_PARAM = "epsg_out";
-
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-            nameValuePairs.add(new BasicNameValuePair(QUERY_PARAM, "geocode"));
-            nameValuePairs.add(new BasicNameValuePair(FORMAT_PARAM, "json"));
-            nameValuePairs.add(new BasicNameValuePair(USERNAME_PARAM, getString(R.string.reittiopas_username)));
-            nameValuePairs.add(new BasicNameValuePair(PASSWORD_PARAM, getString(R.string.reittiopas_password)));
-            nameValuePairs.add(new BasicNameValuePair(COORDINATE_OUTPUT_PARAM, "wgs84"));
-            nameValuePairs.add(new BasicNameValuePair("key", input));
-            String paramString = URLEncodedUtils.format(nameValuePairs, "utf-8");
-
-            URL url = new URL(QUERY_BASE_URL + paramString);
-            Log.d(LOG_TAG, ""+url);
-
-            conn = (HttpURLConnection) url.openConnection();
-            InputStreamReader in = new InputStreamReader(conn.getInputStream());
-
-            // Read the input stream into a String
-            InputStream inputStream = conn.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
-                return null;
-            }
-            resultString = buffer.toString();
-            Log.d(LOG_TAG, resultString);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attempting
-            // to parse it.
-            return null;
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
-                }
-            }
-        }
-        ArrayList<Location> locationList = getPlacesFromJson(resultString);
-
-        if (locationList != null && !locationList.isEmpty()) {
-            String result;
-            for (int i = 0; i < locationList.size(); i++) {
-                result = locationList.get(i).toString();
-                if (result != null && !result.isEmpty()) {
-                    Log.d(LOG_TAG, "adding result "+result);
-                    autoCompleteResultsList.add(result);
-                }
-            }
-        }
-
-        return autoCompleteResultsList;
-    }
-
-    private ArrayList<Location> getPlacesFromJson(String jsonString) {
-        ArrayList<Location> resultList = new ArrayList<>();
-
-        final String TAG_LOCATION_TYPE = "locType";
-        final String TAG_LOCATION_TYPE_ID = "locTypeId";
-        final String TAG_LOCATION_NAME = "name";
-        final String TAG_LOCATION_MATCHED_NAME = "matchedName";
-        final String TAG_LOCATION_LANG = "lang";
-        final String TAG_LOCATION_CITY = "city";
-
-        final String TAG_LOCATION_COORDS = "coords";
-        final String TAG_LOCATION_DETAILS = "details";
-
-        try {
-            JSONArray placesJson = new JSONArray(jsonString);
-            JSONObject place;
-
-            String locType;
-            int locTypeId;
-            String name;
-            String matchedName;
-            String lang;
-            String city;
-            String coords;
-            Coordinates coordinates;
-
-            JSONObject details;
-            LocationDetails locationDetails;
-
-            for (int i = 0; i < placesJson.length(); i++) {
-                place = placesJson.getJSONObject(i);
-
-                locType = place.getString(TAG_LOCATION_TYPE);
-                locTypeId = place.getInt(TAG_LOCATION_TYPE_ID);
-                name = place.getString(TAG_LOCATION_NAME);
-                matchedName = place.getString(TAG_LOCATION_MATCHED_NAME);
-                lang = place.getString(TAG_LOCATION_LANG);
-                city = place.getString(TAG_LOCATION_CITY);
-                coords = place.getString(TAG_LOCATION_COORDS);
-
-                coordinates = readCoordinates(coords);
-
-                details = place.getJSONObject(TAG_LOCATION_DETAILS);
-                locationDetails = getPlaceDetailsFromJson(details);
-
-                resultList.add(new Location(locType, locTypeId, name, matchedName,
-                        lang, city, coordinates, locationDetails));
-            }
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, "Cannot process JSON results", e);
-        }
-
-        Log.d(LOG_TAG, "END OF PARSING");
-
-        return resultList;
-    }
-
-    private LocationDetails getPlaceDetailsFromJson(JSONObject detailsObject) {
-        final String TAG_LOCATION_DETAILS_ADDRESS = "address";
-        final String TAG_LOCATION_DETAILS_CODE = "code";
-        final String TAG_LOCATION_DETAILS_SHORT_CODE= "shortCode";
-        final String TAG_LOCATION_DETAILS_CHANGE_COST = "changeCost";
-        final String TAG_LOCATION_DETAILS_LINES = "lines";
-        final String TAG_LOCATION_DETAILS_TRANSPORT_TYPE_ID = "transport_type_id";
-        final String TAG_LOCATION_DETAILS_HOUSE_NUMBER = "houseNumber";
-        final String TAG_LOCATION_DETAILS_POI_TYPE = "poiType";
-
-        try {
-            String address;
-            String code;
-            String shortCode;
-            double changeCost;
-            JSONArray lines;
-            int transportTypeId;
-            int houseNumber;
-            String poiType;
-
-            LocationDetails details = new LocationDetails();
-
-            if (detailsObject.has(TAG_LOCATION_DETAILS_ADDRESS)) {
-                address = detailsObject.getString(TAG_LOCATION_DETAILS_ADDRESS);
-                details.setAddress(address);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_CODE)) {
-                code = detailsObject.getString(TAG_LOCATION_DETAILS_CODE);
-                details.setCode(code);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_SHORT_CODE)) {
-                shortCode = detailsObject.getString(TAG_LOCATION_DETAILS_SHORT_CODE);
-                details.setShortCode(shortCode);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_CHANGE_COST)) {
-                changeCost = detailsObject.getDouble(TAG_LOCATION_DETAILS_CHANGE_COST);
-                details.setChangeCost(changeCost);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_LINES)) {
-                lines = detailsObject.getJSONArray(TAG_LOCATION_DETAILS_LINES);
-                //details.setLines(lines);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_TRANSPORT_TYPE_ID)) {
-                transportTypeId = detailsObject.getInt(TAG_LOCATION_DETAILS_TRANSPORT_TYPE_ID);
-                details.setTransportTypeId(transportTypeId);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_HOUSE_NUMBER)) {
-                houseNumber = detailsObject.getInt(TAG_LOCATION_DETAILS_HOUSE_NUMBER);
-                details.setHouseNumber(houseNumber);
-            }
-            else if (detailsObject.has(TAG_LOCATION_DETAILS_POI_TYPE)) {
-                poiType = detailsObject.getString(TAG_LOCATION_DETAILS_POI_TYPE);
-                details.setPoiType(poiType);
-            }
-
-            return  details;
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, "Cannot process JSON results", e);
-
-            return null;
-        }
-    }
-
-    /**
-     * Read coordinate information
-     * @param coordinates
-     * @return CustomLocaiton object
-     */
-    private Coordinates readCoordinates(String coordinates) {
-        double latitude = 0; //y
-        double longitude = 0; //x
-
-        try {
-            longitude = Double.parseDouble(coordinates.substring(0, coordinates.indexOf(",")));
-            latitude = Double.parseDouble(coordinates.substring(coordinates.indexOf(",") + 1));
-        }
-        catch (Exception e) {
-        }
-
-        return new Coordinates(latitude, longitude);
+    public void setLocationList(ArrayList<Location> locationList) {
+        this.locationList = locationList;
     }
 
 }
