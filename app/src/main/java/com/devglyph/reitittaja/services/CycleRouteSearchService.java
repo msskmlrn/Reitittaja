9c609c869bfa33fd831924cf2e82a29916ef319b
Miika Sirén <miika.siren@cs.helsinki.fi>
2015-04-16 16:10:29 +0300

    chain append calls when processing strings

--- app/src/main/java/com/devglyph/reitittaja/services/CycleRouteSearchService.java
+++ app/src/main/java/com/devglyph/reitittaja/services/CycleRouteSearchService.java
@@ -111,7 +111,7 @@ public class CycleRouteSearchService extends IntentService {
             String line;
             while ((line = reader.readLine()) != null) {
                 //add new line for debugging
-                buffer.append(line + "\n");
+                buffer.append(line).append("\n");
             }
 
             if (buffer.length() == 0) {
